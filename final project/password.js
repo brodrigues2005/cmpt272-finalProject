a24d93713ecb360c3081c677995fc52936dbad27
brodrigues2005 <rodriguesbrett85@gmail.com>
2024-11-11 12:00:00 -0800

    more changes to the password.js

--- final project/password.js
+++ final project/password.js
@@ -27,7 +27,7 @@ async function fetchHash(passCode){
 }
 
 // async function comparePascodes
-async function comparePascodes(passCode1,passCode2){
+async function comparePascodes(passCode1,passCode2, command){
     const Hash1 = await fetchHash(passCode1);
     const Hash2 = await fetchHash(passCode2);
 
@@ -37,18 +37,47 @@ async function comparePascodes(passCode1,passCode2){
 
 const Passcode = "ANWSB";
 
-//Pull this information from window popup after they select modify or delete
+//Pull this information from window popup after they select modify, delete, or resolve status
 var userEntry = "ANWSB";
 
-comparePascodes(Passcode,userEntry).then(result =>{
+var command = "delete";
+
+comparePascodes(Passcode,userEntry, command).then(result =>{
     EQUAL = result;
     console.log(EQUAL);
 
 
     //if EQUAL === true
-    //Delete report or delete report and bring them to form to make modification
+    //Delete report, make modification, or resolve status
+    if(Equal == true){
+        if(command ===  "DELETE"){
+            //Something like this
+            // document.getElementById("reportList").removeChild(listitem);
+            // delete report;
 
+            //Delete specific report for reports array
+            var reports = JSON.parse(localStorage.getItem('reports'));
+            reports.splice(index,index);
+            localStorage.setItem('reports', JSON.stringify(reports));
+        }
+
+        else if (command === "RESOLVE"){
+            var reports = JSON.parse(localStorage.getItem('reports'));
+            reports[index].status = "Resolved";
+            localStorage.setItem('reports', JSON.stringify(reports));
+        }
+
+        else if (command === "Modify"){
+            
+        }
+        
+    }
     //if EQual === false
     //Window popup saying wrong password was submitted and do nothing
+
+    if(Equal == false){
+        window.prompt("Error: Wrong Password inputed");
+    }
+
 });
 
